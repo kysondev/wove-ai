3ae1ebc13b1e2be5ad29a387c5bf5cee8e932bfc
kysondev <email@kyson.dev>
2025-08-14 00:25:00 -0400

    fix: upate layout prisma import

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -2,14 +2,18 @@ import { ReactNode } from "react";
 import { getUser } from "@/services/user.service";
 import { ChatProvider } from "../../components/providers/ChatProvider";
 import ChatLayout from "../../components/chat/ChatLayout";
-import { User } from "@/generated/prisma-client";
+import { User } from "@prisma/client";
 
-export default async function ChatRouteLayout({ children }: { children: ReactNode }) {
-	const result = await getUser();
-	const user = result.success ? result.data : null;
-	return (
-		<ChatProvider user={user as User}>
-			<ChatLayout>{children}</ChatLayout>
-		</ChatProvider>
-	);
+export default async function ChatRouteLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const result = await getUser();
+  const user = result.success ? result.data : null;
+  return (
+    <ChatProvider user={user as User}>
+      <ChatLayout>{children}</ChatLayout>
+    </ChatProvider>
+  );
 }
